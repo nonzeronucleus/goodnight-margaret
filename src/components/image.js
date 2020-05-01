39dc0638690eccbea13345fcf29005121f42e3f7
nonzeronucleus <gitkraken@ianplumb.org>
2020-05-01 21:34:00 +0100

    Adds basic band info

--- src/components/image.js
+++ src/components/image.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
+import styled from 'styled-components';
 
 /*
  * This component is built using `gatsby-image` to automatically serve optimized
@@ -13,10 +14,16 @@ import Img from "gatsby-image"
  * - `useStaticQuery`: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
+
+const StyledImage = styled(Img)`
+    filter: blur(3px);
+`;
+
+
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+      placeholderImage: file(relativePath: { eq: "band.JPG" }) {
         childImageSharp {
           fluid(maxWidth: 300) {
             ...GatsbyImageSharpFluid
@@ -26,7 +33,9 @@ const Image = () => {
     }
   `)
 
-  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+  return <StyledImage fluid={data.placeholderImage.childImageSharp.fluid} />
 }
 
-export default Image
+
+
+export default Image;
