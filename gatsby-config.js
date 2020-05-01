eddb28577a5712567c94315543ad744ab537bc9a
nonzeronucleus <gitkraken@ianplumb.org>
2020-05-01 19:40:01 +0100

    Adds new title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Goodnight Margaret`,
+    description: `A band for all seasons (except autumn).`,
     author: `@gatsbyjs`,
   },
   plugins: [
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,11 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Goodnight, Margaret</p>
+    <h1>We are Goodnight Margaret</h1>
     <p>We are great</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
